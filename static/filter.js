dc08e4aa58d3bb56e9d1741967e78946007b3c03
Filip Hnízdo <filip@bluejumpers.com>
2016-12-17 00:36:00 +0000

    More iOS zoom fixes

--- static/filter.js
+++ static/filter.js
@@ -267,8 +267,3 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 
 });
-
-var $viewportMeta = $('meta[name="viewport"]');
-$('input, select, textarea').bind('focus blur', function (event) {
-  $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
-});
