7db13666323cef6da664e574d87232ec4a9482c2
Filip Hnízdo <filip@bluejumpers.com>
2016-12-17 00:33:33 +0000

    iOS zooming issue

--- static/filter.js
+++ static/filter.js
@@ -267,3 +267,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
   }
 
 });
+
+var $viewportMeta = $('meta[name="viewport"]');
+$('input, select, textarea').bind('focus blur', function (event) {
+  $viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
+});
